f362febdc82eed55d38823c852a447a5d9c8a8a1
Jimena Soledad Ruiz <jimenasoledadruiz@gmail.com>
2025-11-22 14:54:12 -0300

    hasta aca preentrega

---
+++ src/App.jsx
@@ -0,0 +1,26 @@
+
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import './App.css';
+import { ItemListContainer } from './components/ItemListContainer/ItemListContainer';
+import { ItemDetailContainer } from './components/ItemDetailContainer/ItemDetailContainer';
+import { CartProvider } from './context/CartContext/CartProvider';
+import { Nav } from './components/Nav/Nav';
+
+function App() {
+
+  return (
+    <>
+      <BrowserRouter>
+        <CartProvider>
+          <Nav/>
+          <Routes>
+            <Route path="/" element={<ItemListContainer/>}/>
+            <Route path="/detail/:id" element={<ItemDetailContainer/>}/>
+          </Routes>
+        </CartProvider>
+      </BrowserRouter>
+    </>
+  );
+}
+
+export default App;
