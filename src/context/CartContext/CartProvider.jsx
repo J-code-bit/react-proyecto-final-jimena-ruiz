a197dbf733c1f94583b8adaf27b3bfc88ce81e47
Jimena Soledad Ruiz <jimenasoledadruiz@gmail.com>
2025-11-25 18:34:03 -0300

    agregado de funciones en CartProvider

--- src/context/CartContext/CartProvider.jsx
+++ src/context/CartContext/CartProvider.jsx
@@ -5,18 +5,31 @@ export const CartProvider = ({children}) => {
     const [cart, setCart] = useState([]);
 
     const exists = (id) => {
-        const exist = cart.some(p => p.id === id);
+        const exist = cart.some((p) => p.id === id);
         return exist;
     };
 
     const addItem = (item) => {
         if(exists(item.id)){
-            alert("El producto ya existe en el carrito");
-            return;
+            const updatedCart = cart.map((prod) => {
+                if (prod.id === item.id) {
+                    return { ...prod, quantity: prod.quantity + item.quantity };
+                } else {
+                    return prod;
+                }
+            });
+            setCart(updatedCart);
+            alert(`Agregado al carrito`);
+            } else {
+            setCart([...cart, item]);
+            alert(`${item.name} agregado`);
         }
+    };
 
-        setCart([...cart,item]);
-        alert(`${item.name} agregado`);
+    const deleteItem = (id) => {
+        const filtered = cart.filter((p) => p.id !== id);
+        setCart(filtered);
+        alert("Producto eliminado");
     };
 
     const clearCart = () => {
@@ -24,8 +37,22 @@ export const CartProvider = ({children}) => {
     };
 
     const getTotalItems =() => {
-        if (cart.length){
-            return cart.length;
+        const totalItems = cart.reduce((acc, p) => acc + p.quantity, 0);
+        return totalItems;
+    };
+
+    const total = () => {
+        const total = cart.reduce((acc, p) => acc + p.price * p.quantity, 0);
+    
+        return Math.round(total * 100) / 100;
+    };
+    
+    const checkout = () => {
+        const ok = confirm("¿Serguro que quiere finalizar la compra?");
+    
+        if (ok) {
+            alert("¡Compra realizada con éxito!");
+            clearCart();
         }
     };
 
@@ -34,7 +61,12 @@ export const CartProvider = ({children}) => {
         addItem,
         clearCart,
         getTotalItems,
+        deleteItem,
+        total,
+        checkout,
     };
     
     return <CartContext.Provider value = {values}>{children}</CartContext.Provider>;
 };
+
+
