96fcc05d4ad09447193638b33781c775770442e5
Jimena Soledad Ruiz <jimenasoledadruiz@gmail.com>
2025-11-28 13:36:04 -0300

    se agrego AuthContext y RutasProtegidas

--- src/App.jsx
+++ src/App.jsx
@@ -5,9 +5,12 @@ import { ItemListContainer } from './components/ItemListContainer/ItemListContai
 import { ItemDetailContainer } from './components/ItemDetailContainer/ItemDetailContainer';
 import { CartProvider } from './context/CartContext/CartProvider';
 import { Cart } from './components/Cart/Cart';
-import { Header } from './components/Header/Header';
 import { Footer } from './components/Footer/Footer';
 import {ProductFormContainer} from './components/adminComponents/ProductFormContainer/ProductFormContainer';
+import {MainLayout} from "./layouts/MainLayout";
+import {AdminLayout} from "./layouts/AdminLayout";
+import {RutaProtegida} from "./components/RutaProtegida/RutaProtegida";
+import {Login} from "./components/Login/Login";
 
 
 function App() {
@@ -16,9 +19,9 @@ function App() {
     <>
       <BrowserRouter>
         <CartProvider>
-          <Header/>
           <Routes>
-          <Route
+            <Route element={<MainLayout/>}>
+            <Route
               path="/"
               element={<ItemListContainer titulo={"Bienvenidos"} />}
             />
@@ -28,7 +31,19 @@ function App() {
             />
             <Route path="/detail/:id" element={<ItemDetailContainer />} />
             <Route path="/carrito" element={<Cart/>}/>
-            <Route path="/admin" element={<ProductFormContainer />} />
+            </Route>
+
+            <Route path="/admin" element={<AdminLayout/>}>
+              <Route index element={<Login/>}/>
+              <Route 
+                path="alta-productos" 
+                element={
+                  <RutaProtegida>
+                    <ProductFormContainer/>
+                  </RutaProtegida>
+                }
+              />
+            </Route>
           </Routes>
           <Footer/>
         </CartProvider>
